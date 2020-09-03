97688f389d2b8d82acb9ae204835e9e769483222
vbenoit <vincent.benoit@scle.fr>
2020-09-03 17:42:53 +0200

    modification des sources

--- src/hello.c
+++ src/hello.c
@@ -8,7 +8,7 @@ int main(int argc, char** argv) {
 	int ret = 0;
 	char str[64];
 	memset(str, 0, sizeof(str));
-	ret = world(sizeof(str), str);
+	world(sizeof(str), str);
 	printf("Hello World => %s\n", str);
 	return ret;
 }
